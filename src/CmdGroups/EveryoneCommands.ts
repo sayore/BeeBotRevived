8715239d43f0df823b5cd89659dab532e24388a5
Sayore <bkrippner@yahoo.de>
2023-07-17 10:27:27 +0200

    Message Author unknown??

--- src/CmdGroups/EveryoneCommands.ts
+++ src/CmdGroups/EveryoneCommands.ts
@@ -64,7 +64,7 @@ export let EveryoneCommands : ICommand[] = [
                 if(redirectChannel && redirectChannel.isText()) {
                     msg.channel.send("Deine Nachricht wurde an die aktiven mods weitergeleitet kleinen moment bitte!")
                     var embed = new Discord.MessageEmbed();
-                    embed.setAuthor({name:user.tag, iconURL: msg.member.avatarURL({dynamic: true, size: 512})});
+                    embed.setAuthor({name:user.tag ?? "Unknown"});
                     embed.setDescription(msg.content);
                     embed.setColor("RANDOM");
                     embed.setTimestamp(msg.createdTimestamp);
