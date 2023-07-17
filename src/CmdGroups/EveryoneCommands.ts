5e2dc66b076e959b04d1b9687365fe20196d4af8
Sayore <bkrippner@yahoo.de>
2023-07-17 10:29:41 +0200

    Test

--- src/CmdGroups/EveryoneCommands.ts
+++ src/CmdGroups/EveryoneCommands.ts
@@ -64,7 +64,7 @@ export let EveryoneCommands : ICommand[] = [
                 if(redirectChannel && redirectChannel.isText()) {
                     msg.channel.send("Deine Nachricht wurde an die aktiven mods weitergeleitet kleinen moment bitte!")
                     var embed = new Discord.MessageEmbed();
-                    embed.setAuthor({name:user.tag ?? "Unknown"});
+                    embed.setAuthor({name:"Unknown"});
                     embed.setDescription(msg.content);
                     embed.setColor("RANDOM");
                     embed.setTimestamp(msg.createdTimestamp);
