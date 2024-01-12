496fe1d59698e0f656ebb69adedcec91f599b84b
Sayore <bkrippner@yahoo.de>
2024-01-12 21:00:12 +0100

    removed midword filter

--- src/CmdGroups/EveryoneCommands.ts
+++ src/CmdGroups/EveryoneCommands.ts
@@ -236,10 +236,10 @@ export let EveryoneCommands : ICommand[] = [
             if(msg.content.toLowerCase()=="nh") {sendNhMsg();msg.delete();return;}
             if(msg.content.toLowerCase().includes("nh!")) {sendNhMsg();msg.delete();return;}
             if(msg.content.toLowerCase().includes("-. ....")) {sendNhMsg();msg.delete();return;}
-            if(msg.content.toLowerCase().includes("nh")) {sendNhMsg();msg.delete();return;}
+            //if(msg.content.toLowerCase().includes("nh")) {sendNhMsg();msg.delete();return;}
             if(msg.content.toLowerCase().includes("nh?")) {sendNhMsg();msg.delete();return;}
-            if(msg.content.toLowerCase().includes("ոh")) {sendNhMsg();msg.delete();return;}
-            if(msg.content.toLowerCase().includes("ոhh")) {sendNhMsg();msg.delete();return;}
+            //if(msg.content.toLowerCase().includes("ոh")) {sendNhMsg();msg.delete();return;}
+            //if(msg.content.toLowerCase().includes("ոhh")) {sendNhMsg();msg.delete();return;}
             if(msg.content.toLowerCase().includes("*nh*")) {sendNhMsg();msg.delete();return;}
             if(msg.content.toLowerCase().includes("_nh_")) {sendNhMsg();msg.delete();return;}
             if(msg.content.toLowerCase().includes(" n....h")) {sendNhMsg();msg.delete();return;}
